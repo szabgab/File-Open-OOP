4c65daf1aadd2fbc92536781d7398e9f137f936b
Gabor Szabo <gabor@szabgab.com>
2011-12-21 09:34:37 +0200

    add oopen and constructor

--- lib/File/Open/OOP.pm
+++ lib/File/Open/OOP.pm
@@ -2,5 +2,21 @@ package File::Open::OOP;
 use strict;
 use warnings;
 
+our $VERSION = '0.01';
+
+use File::Open qw(fopen);
+
+sub new {
+	my ($class) = @_;
+	my $self = {};
+
+	return bless $self, $class;
+}
+
+sub oopen {
+	#fopen
+	return __PACKAGE__->new;
+}
+
 
 1;
--- t/01_basics.t
+++ t/01_basics.t
@@ -3,7 +3,12 @@ use warnings;
 
 use Test::More;
 
-plan tests => 1;
+plan tests => 2;
 
 use_ok('File::Open::OOP');
+
+my $filename = 'Makefile.PL';
+
+my $fh = File::Open::OOP::oopen($filename);
+isa_ok $fh, 'File::Open::OOP';
 
